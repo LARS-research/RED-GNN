e064161e8d2b8b3e0ed17d7a746cbcb43b3c327e
yzhangee <yzhangee@connect.ust.hk>
2022-02-15 11:00:33 +0800

    init code

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # RED-GNN
-Knowledge Graph Reasoning with Relational Digraph. WebConf 2022
+The code for our paper ["Knowledge Graph Reasoning with Relational Digraph", which has been accepted by ](https://arxiv.org/pdf/2108.06040.pdf)WebConf 2022.
+
+
+
+## Instructions
+
+A quick instruction is given for readers to reproduce the whole process.
+
+
+
+Requirements 
+
+- pytorch  1.9.1+cu102
+- torch_scatter 2.0.9
+
+
+
+For transductive reasoning
+
+​	cd transductive
+
+​	python -W ignore train.py --data_path=data/WN18RR
+
+
+
+For inductive reasoning
+
+​	cd inductive
+
+​	python -W ignore train.py --data_path=data/WN18RR_v1
+
+
+
+### Transductive results
+
+| Metrics    | Family | UMLS | WN18RR | FB15k-237 | NELL-995 |
+| ---------- | ------ | ---- | ------ | --------- | -------- |
+| MRR        | .992   | .964 | .533   | .374      | .543     |
+| Hit@1 (%)  | 98.8   | 94.6 | 48.5   | 28.3      | 47.6     |
+| Hit@10 (%) | 99.7   | 99.0 | 62.4   | 55.8      | 65.1     |
+
+### Inductive results
+
+We use the full set of negative samples in evaluating the inductive results. This is different from the setting of 50 negative samples in [GraIL](https://arxiv.org/pdf/1911.06962.pdf).
+
+| metrics    | WN-V1 | WN-V2 | WN-V3 | WN-V4 | FB-V1 | FB-V2 | FB-V3 | FB-V4 | NL-V1 | NL-V2 | NL-V3 | NL-V4 |
+| ---------- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- | ----- |
+| MRR        | .701  | .690  | .427  | .651  | .369  | .469  | .445  | .442  | .637  | .419  | .436  | .363  |
+| Hit@1 (%)  | 65.3  | 63.3  | 36.8  | 60.6  | 30.2  | 38.1  | 35.1  | 34.0  | 52.5  | 31.9  | 34.5  | 25.9  |
+| Hit@10 (%) | 79.9  | 78.0  | 52.4  | 72.1  | 48.3  | 62.9  | 60.3  | 62.1  | 86.6  | 60.1  | 59.4  | 55.6  |
+
+
+
---
+++ inductive/base_model.py
@@ -0,0 +1,120 @@
+import torch
+import numpy as np
+import time
+
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ExponentialLR
+from models import RED_GNN_induc
+from utils import cal_ranks, cal_performance
+
+class BaseModel(object):
+    def __init__(self, args, loader):
+        self.model = RED_GNN_induc(args, loader)
+        self.model.cuda()
+
+        self.loader = loader
+        self.n_ent = loader.n_ent
+        self.n_ent_ind = loader.n_ent_ind
+        self.n_batch = args.n_batch
+
+        self.n_train = loader.n_train
+        self.n_valid = loader.n_valid
+        self.n_test  = loader.n_test
+        self.n_layer = args.n_layer
+
+        self.optimizer = Adam(self.model.parameters(), lr=args.lr, weight_decay=args.lamb)
+        self.scheduler = ExponentialLR(self.optimizer, args.decay_rate)
+        self.t_time = 0
+
+    def train_batch(self,):
+        epoch_loss = 0
+        i = 0
+
+        batch_size = self.n_batch
+        n_batch = self.n_train // batch_size + (self.n_train % batch_size > 0)
+
+        t_time = time.time()
+        self.model.train()
+        for i in range(n_batch):
+            start = i*batch_size
+            end = min(self.n_train, (i+1)*batch_size)
+            batch_idx = np.arange(start, end)
+            triple = self.loader.get_batch(batch_idx)
+
+            self.model.zero_grad()
+            scores = self.model(triple[:,0], triple[:,1])
+
+            pos_scores = scores[[torch.arange(len(scores)).cuda(),torch.LongTensor(triple[:,2]).cuda()]]
+            max_n = torch.max(scores, 1, keepdim=True)[0]
+            loss = torch.sum(- pos_scores + max_n + torch.log(torch.sum(torch.exp(scores - max_n),1)))
+            loss.backward()
+            self.optimizer.step()
+
+            # avoid NaN
+            for p in self.model.parameters():
+                X = p.data.clone()
+                flag = X != X
+                X[flag] = np.random.random()
+                p.data.copy_(X)
+            epoch_loss += loss.item()
+        self.scheduler.step()
+        self.t_time += time.time() - t_time
+
+        valid_mrr, out_str = self.evaluate()
+        return valid_mrr, out_str
+
+    def evaluate(self, ):
+        batch_size = self.n_batch
+
+        n_data = self.n_valid
+        n_batch = n_data // batch_size + (n_data % batch_size > 0)
+        ranking = []
+        self.model.eval()
+        i_time = time.time()
+        for i in range(n_batch):
+            start = i*batch_size
+            end = min(n_data, (i+1)*batch_size)
+            batch_idx = np.arange(start, end)
+            subs, rels, objs = self.loader.get_batch(batch_idx, data='valid')
+            scores = self.model(subs, rels).data.cpu().numpy()
+            filters = []
+            for i in range(len(subs)):
+                filt = self.loader.val_filters[(subs[i], rels[i])]
+                filt_1hot = np.zeros((self.n_ent, ))
+                filt_1hot[np.array(filt)] = 1
+                filters.append(filt_1hot)
+             
+            filters = np.array(filters)
+            ranks = cal_ranks(scores, objs, filters)
+            ranking += ranks
+        ranking = np.array(ranking)
+        v_mrr, v_h1, v_h10 = cal_performance(ranking)
+
+
+        n_data = self.n_test
+        n_batch = n_data // batch_size + (n_data % batch_size > 0)
+        ranking = []
+        self.model.eval()
+        for i in range(n_batch):
+            start = i*batch_size
+            end = min(n_data, (i+1)*batch_size)
+            batch_idx = np.arange(start, end)
+            subs, rels, objs = self.loader.get_batch(batch_idx, data='test')
+            scores = self.model(subs, rels, 'inductive').data.cpu().numpy()
+            filters = []
+            for i in range(len(subs)):
+                filt = self.loader.tst_filters[(subs[i], rels[i])]
+                filt_1hot = np.zeros((self.n_ent_ind, ))
+                filt_1hot[np.array(filt)] = 1
+                filters.append(filt_1hot)
+             
+            filters = np.array(filters)
+            ranks = cal_ranks(scores, objs, filters)
+            ranking += ranks
+        ranking = np.array(ranking)
+        t_mrr, t_h1, t_h10 = cal_performance(ranking)
+        i_time = time.time() - i_time
+
+        out_str = '[VALID] MRR:%.4f H@1:%.4f H@10:%.4f\t [TEST] MRR:%.4f H@1:%.4f H@10:%.4f \t[TIME] train:%.4f inference:%.4f\n'%(v_mrr, v_h1, v_h10, t_mrr, t_h1, t_h10, self.t_time, i_time)
+        return v_mrr, out_str
+
